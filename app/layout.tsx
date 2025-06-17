7c9b83a8fb4abe0fb6ba726dc5bdb6bf48e53b43
Adam Chrimes <iChrimes@live.co.uk>
2025-06-17 20:26:02 +0100

    fix: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "chrimes.dev",
+  title: "chrimes.dev - Website Design & Development",
   description: "Website design & development by chrimes.dev",
 };
 
